7c2ec93e2ec674c5ea0d46db0bae5bbb1ab3c544
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-09-01 08:59:15 +0900

    ログイン処理

--- components/Auth.js
+++ components/Auth.js
@@ -76,39 +76,48 @@ export default function Auth() {
             alt="Workflow"
           />
           <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
-            Sign in to your account
+            {isLogin ? "Login" : "Sign up"}
           </h2>
         </div>
-        <form className="mt-8 space-y-6" action="#" method="POST">
+        <form className="mt-8 space-y-6" onSubmit={authUser}>
           <input type="hidden" name="remember" value="true" />
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
               <input
-                id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
+                name="username"
+                type="text"
+                autoComplete="username"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Email address"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
               />
             </div>
             <div>
               <input
-                id="password"
                 name="password"
                 type="password"
                 autoComplete="current-password"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
               />
             </div>
           </div>
 
           <div className="flex items-center justify-center">
             <div className="text-sm">
-              <span className="cursor-pointer font-medium text-white hover:text-indigo-500">
+              <span
+                onClick={() => setIsLogin(!isLogin)}
+                className="cursor-pointer font-medium text-white hover:text-indigo-500"
+              >
                 change mode ?
               </span>
             </div>
@@ -134,7 +143,7 @@ export default function Auth() {
                   />
                 </svg>
               </span>
-              Sign in
+              {isLogin ? "Login with JWT" : "Create new user"}
             </button>
           </div>
         </form>
