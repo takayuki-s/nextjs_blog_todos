6bbbd304b3a1fdcefbc8cf9e7a638f38f6d6551f
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-09-02 08:59:13 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Auth from "../components/Auth";
 import Layout from "../components/Layout";
 
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_RESTAPI_URL);
   return (
     <Layout title="Login">
       <Auth />
