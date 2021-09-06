839d7929e5f027f10921a464d5730f5db8ad888a
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-09-07 06:37:33 +0900

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children, title = "Default title" }) {
       <Head>
         <title>{title}</title>
       </Head>
-      <main className="flex flex-1 justify-center items-center w-screen flex-col">
+      <main className="flex flex-1 justify-center items-center w-screen flex-col text-white">
         {children}
       </main>
       <footer className="w-full h-6 flex justify-center items-center text-gray-500 text-sm">
