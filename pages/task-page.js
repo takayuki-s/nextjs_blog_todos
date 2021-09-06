8e816206a8a67ec37d3c1b92b4bab4af8a5c3940
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-09-06 09:18:00 +0900

    不具合修正
    return抜け
    Linkが"link"になってた

--- pages/task-page.js
+++ pages/task-page.js
@@ -1,26 +1,28 @@
 import Layout from "../components/Layout";
-import link from "next/link";
+import Link from "next/link";
 
 export default function TaskPage() {
-  <Layout title="TaskPage">
-    <Link href="/main-page">
-      <div className="w-6 h-6 mr-3">
-        <svg
-          className="w-6 h-6 mr-3"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
-          ></path>
-        </svg>
-        <span>Back to main page</span>
-      </div>
-    </Link>
-  </Layout>;
+  return (
+    <Layout title="TaskPage">
+      <Link href="/main-page">
+        <div className="w-6 h-6 mr-3">
+          <svg
+            className="w-6 h-6 mr-3"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+            ></path>
+          </svg>
+          <span>Back to main page</span>
+        </div>
+      </Link>
+    </Layout>
+  );
 }
