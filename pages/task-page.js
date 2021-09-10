5f726a90224e6682bbdf42b563f7bad92201e8f1
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-09-10 09:13:07 +0900

    Task表示されない不具合解消
    const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`
    
    の最後"task/"の"/"がいらんかった！！！

--- pages/task-page.js
+++ pages/task-page.js
@@ -6,56 +6,24 @@ import useSWR from "swr";
 import { useEffect } from "react";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
-const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
-
-// export default function TaskPage({ staticfilteredTasks }) {
-//   const { data: tasks, mutate } = useSWR(apiUrl, fetcher, {
-//     initialData: staticfilteredTasks,
-//   });
-//   const filteredTasks = tasks?.sort(
-//     (a, b) => new Date(b.created_at) - new Date(a.created_at)
-//   );
-//   useEffect(() => {
-//     mutate();
-//   }, []);
-
-//   return (
-//     <Layout title="Task page">
-//       <ul>
-//         {filteredTasks &&
-//           filteredTasks.map((task) => <Task key={task.id} task={task} />)}
-//       </ul>
-//       <Link href="/main-page">
-//         <div className="flex cursor-pointer mt-12">
-//           <svg
-//             className="w-6 h-6 mr-3"
-//             fill="none"
-//             stroke="currentColor"
-//             viewBox="0 0 24 24"
-//             xmlns="http://www.w3.org/2000/svg"
-//           >
-//             <path
-//               strokeLinecap="round"
-//               strokeLinejoin="round"
-//               strokeWidth="2"
-//               d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
-//             ></path>
-//           </svg>
-//           <span>Back to main page</span>
-//         </div>
-//       </Link>
-//     </Layout>
-//   );
-// }
+const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task`;
 
 export default function TaskPage({ staticfilteredTasks }) {
+  const { data: tasks, mutate } = useSWR(apiUrl, fetcher, {
+    initialData: staticfilteredTasks,
+  });
+  const filteredTasks = tasks?.sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  );
+  useEffect(() => {
+    mutate();
+  }, []);
+
   return (
-    <Layout title="BlogPage">
+    <Layout title="Task page">
       <ul>
-        {staticfilteredTasks &&
-          staticfilteredTasks.map((task) => (
-            <Task key={task.id} task={task} />
-          ))}{" "}
+        {filteredTasks &&
+          filteredTasks.map((task) => <Task key={task.id} task={task} />)}
       </ul>
       <Link href="/main-page">
         <div className="flex cursor-pointer mt-12">
@@ -79,6 +47,38 @@ export default function TaskPage({ staticfilteredTasks }) {
     </Layout>
   );
 }
+
+// export default function TaskPage({ staticfilteredTasks }) {
+//   return (
+//     <Layout title="Task Page">
+//       <ul>
+//         {staticfilteredTasks &&
+//           staticfilteredTasks.map((task) => (
+//             <Task key={task.id} task={task} />
+//           ))}{" "}
+//       </ul>
+//       <Link href="/main-page">
+//         <div className="flex cursor-pointer mt-12">
+//           <svg
+//             className="w-6 h-6 mr-3"
+//             fill="none"
+//             stroke="currentColor"
+//             viewBox="0 0 24 24"
+//             xmlns="http://www.w3.org/2000/svg"
+//           >
+//             <path
+//               strokeLinecap="round"
+//               strokeLinejoin="round"
+//               strokeWidth="2"
+//               d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+//             ></path>
+//           </svg>
+//           <span>Back to main page</span>
+//         </div>
+//       </Link>
+//     </Layout>
+//   );
+// }
 
 export async function getStaticProps() {
   const staticfilteredTasks = await getAllTasksData();
