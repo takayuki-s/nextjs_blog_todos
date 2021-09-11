2ffeb99ee6d2b1a345211a8c985282f090d6cdcb
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-09-12 06:39:47 +0900

    指定したidにトークンを持たせてアクセス

--- components/Task.js
+++ components/Task.js
@@ -1,6 +1,22 @@
 import Link from "next/link";
+import { Cookie } from "universal-cookie";
+
+const cookie = new Cookie();
 
 export default function Task({ task }) {
+  const deleteTask = async () => {
+    await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks/${task.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${cookie.get("access_token")}`,
+      },
+    }).then(() => {
+      if (resizeBy.status === 401) {
+        alert("JWT Token not valid");
+      }
+    });
+  };
   return (
     <div>
       <span>{task.id}</span>
