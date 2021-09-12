d6b8ee819c941e23481e807a6b93f247565c6e5d
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-09-13 06:27:12 +0900

    ビュー崩れ修正
    trashアイコンがデカかった

--- components/Task.js
+++ components/Task.js
@@ -30,7 +30,7 @@ export default function Task({ task, taskDeleted }) {
       <div className="float-right ml-20">
         <svg
           onClick={deleteTask}
-          className="w-6 h-6 mr-2 cursor-pointer"
+          className="w-5 h-5 mr-2 cursor-pointer"
           fill="none"
           stroke="currentColor"
           viewBox="0 0 24 24"
@@ -41,7 +41,7 @@ export default function Task({ task, taskDeleted }) {
             strokeLinejoin="round"
             strokeWidth="2"
             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-          ></path>
+          />
         </svg>
       </div>
     </div>
